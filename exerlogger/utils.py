5b28745e36846d8f9bb7fc1c0d585478174c8706
Jakub Mirovsky <jakub.mirovsky@gmail.com>
2020-04-11 08:27:32 +0200

    fix calendar bugy, nyni zobrazuje trainings ve spravne mesice

--- exerlogger/utils.py
+++ exerlogger/utils.py
@@ -41,7 +41,7 @@ class Calendar(HTMLCalendar):
     # filter workouts by year and month
     def formatmonth(self, username, withyear=True):
         workouts = Workout.objects.filter(date__year=self.year, date__month=self.month, user=username)
-        trainings = Training.objects.filter(lesson=username.lesson)
+        trainings = Training.objects.filter(date__year=self.year, date__month=self.month, lesson=username.lesson)
 
         cal = f'<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
         cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
