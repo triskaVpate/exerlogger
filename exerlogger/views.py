8c06fbb7f7c8474076370cb495cb2e0298f1c00e
Jakub Mirovsky <jakub.mirovsky@gmail.com>
2020-04-12 09:12:46 +0200

    fix for signup page, small tweaks

--- exerlogger/views.py
+++ exerlogger/views.py
@@ -89,14 +89,19 @@ def user_homepage(request):
 
 
 def signup_view(request):
-    form = CustomUserCreationForm(request.POST)
-    if form.is_valid():
-        form.save()
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password1')
-        user = authenticate(username=username, password=password)
-        login(request, user)
-        return redirect('home')
+    # show empty form
+    form = CustomUserCreationForm()
+    # if post, do the thing
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # after save lets authenticate user, login him and redirect to homepage
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
     return render(request, 'signup.html', {'form': form})
 
 
